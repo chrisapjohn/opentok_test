4fccde8669ef974cea0e3d30fe75ef668ec32712
Chris Upjohn <chrisupjohn@gmail.com>
2014-02-04 17:49:01 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpentokTest::Application.config.secret_token = '88535599e92ce64dbd09d84a657210884342e0079f1c37bc7f9a5662e4fb7f955a2470f5db912ea1371671c410841ee930ce69ae4c6c2917dad5edb1e5c50bdf'
